f2d1a2b43ce199171bff175db969a362f3a20f9a
Anthony Ishkan <anthony.ishkan@gmail.com>
2016-12-31 16:34:21 +1100

    Update test scripts and files.

---
+++ pytooth/tests/config.py
@@ -0,0 +1,49 @@
+"""Provides helper methods for handling configuration definition and validation.
+"""
+
+import json
+
+from pytooth.tests.errors import ConfigurationError
+
+
+def get_config(filename):
+    """Reads in the specified JSON-formatted file and returns it as a dictionary
+    using the ``json`` module. Throws ``dishtube.errors.ConfigurationError``
+    if an error occurred.
+    """
+
+    try:
+        with open(filename, 'r') as f:
+            return json.load(f)
+    except Exception as e:
+        raise ConfigurationError(e)
+
+def validate_config(config):
+    """Validates the supplied configuration. Validation does not include checks
+    for valid values parses the data against a defined configuration and
+    returns a dictionary if validation succeeded.
+    Throws a ``ConfigurationError`` error if validation failed.
+    """
+
+    if not isinstance(config, dict):
+        raise TypeError("config must be a dictionary.")
+
+    # A JSON-formatted file containing logging configuration.
+    arg = config.get("logging", {})
+    if not isinstance(arg, dict):
+        raise ConfigurationError("'logging' must be dictionary.")
+
+    # Interval in seconds to wait between adapter checks
+    arg = config.get("retryinterval", 0)
+    try:
+        if arg < 5:
+            raise ValueError()
+    except Exception:
+        raise ConfigurationError("'retryinterval' must be at least 5.")
+
+    # Optional string properties
+    for s in ["preferredaddress"]:
+        arg = subconfig.get(s, None)
+        if not isinstance(arg, str):
+            raise ConfigurationError(
+                "'{}' must be a string.".format(s))
---
+++ pytooth/tests/main.py
@@ -0,0 +1,74 @@
+"""Defines a test entry point."""
+
+import argparse
+from functools import partial
+import logging
+import logging.config
+import signal
+import sys
+
+from pytooth.a2dp import AdvancedAudioProfile
+from pytooth.adapters import OpenPairableAdapter
+from pytooth.gi import GtkMainLoop
+import pytooth.tests.config
+from pytooth.tests.errors import ConfigurationError
+
+
+_closing = False
+
+def signal_handler(signum, frame):
+    global _closing
+    _closing = True
+
+def try_exit(gtkloop, a2dp):
+    global _closing
+    if _closing:
+        a2dp.stop()
+        gtkloop.stop()
+        logging.info("Gracefully stopped. Have a nice day.")
+
+def main():
+    # process (CLI) args
+    parser = argparse.ArgumentParser(
+        prog="pytooth-test" if len(args) == 0 else args[0],
+        description="Test app for pytooth library.")
+    parser.add_argument(
+        "--config", "-c",
+        type=str,
+        required=True)
+    v = vars(parser.parse_args(args[1:]))
+
+    # process configuration file
+    config = pytooth.tests.config.get_config(filename=v["config"])
+    try:
+        pytooth.tests.config.validate_config(config=config)
+    except ConfigurationError as e:
+        print("ERROR: Bad configuration found - {}".format(e))
+        sys.exit(1)
+
+    # apply logging configuration
+    if "logging" in config:
+        try:
+            logging.config.dictConfig(config["logging"])
+            logging.info("Applied logging configuration.")
+        except Exception as e:
+            print("WARNING! Could not parse logging configuration, logging may "
+                "not be configured properly - {}".format(e))
+
+    # A2DP
+    a2dp = AdvancedAudioProfile(
+        adapter_class=OpenPairableAdapter,
+        preferred_address=config["preferredaddress"],
+        retry_interval=config["retryinterval"],
+        io_loop=IOLoop.instance())
+
+    # run the test app
+    logging.info("Running...")
+    gtkloop = GtkMainLoop(io_loop=IOLoop.instance())
+    signal.signal(signal.SIGINT, signal_handler)
+    PeriodicCallback(partial(
+        try_exit,
+        gtkloop=gtkloop,
+        a2dp=a2dp), 500).start()
+    a2dp.start()
+    gtkloop.start()
