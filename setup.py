53179fe04e4924cb6246c76c3c46df3a0689cac6
Anthony Ishkan <anthony.ishkan@gmail.com>
2017-02-25 20:39:24 +1100

    Fix setup.py and dependencies.txt

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Setup definition."""
+"""Setup entry point."""
 
 import os
 import site
@@ -6,90 +6,86 @@ import site
 from setuptools import setup, find_packages
 
 
-def main():
-    """Setup entry point."""
+# to get the long description
+with open('README.md') as f:
+    long_description = f.read()
 
-    # to get the long description
-    with open('README.md') as f:
-        long_description = f.read()
+# parse the requirements files
+with open('requirements.txt') as f:
+    install_requirements = f.read().split("\n")
+with open('requirements_dev.txt') as f:
+    test_requirements = f.read().split("\n")
 
-    # parse the requirements files
-    with open('requirements.txt') as f:
-        install_requirements = f.read().split("\n")
-    with open('requirements_dev.txt') as f:
-        test_requirements = f.read().split("\n")
+# pre-reqs
+packages_dir = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    "packages/")
+python_dir = os.path.join(site.USER_SITE, "../").replace("/", "\\/")
 
-    # pre-reqs
-    packages_dir = os.path.join(
-        os.path.dirname(os.path.realpath(__file__)),
-        "packages/")
-    python_dir = os.path.join(site.USER_SITE, "../")
+# parse dependency locations
+with open('dependencies.txt') as f:
+    dependency_requirements = f.read().replace(
+        "{{ PACKAGES_DIR }}", packages_dir).split("\n")
 
-    # parse dependency locations
-    with open('dependencies.txt') as f:
-        dependency_requirements = f.read().replace(
-            "{{ PACKAGES_DIR }}", packages_dir).split("\n")
+# do GObject install
+os.system(
+    "cd "+packages_dir+"; "
+    "tar xf pygobject-3.22.0.tar.xz; "
+    "cd pygobject-3.22.0; "
+    "sed -i '13273s/${prefix}/"+python_dir+"/' configure; "
+    "sed -i '13275s/${exec_prefix}/"+python_dir+"/' configure; "
+    "./configure; "
+    "make; "
+    "sudo make install; "
+    "python setup.py install; ")
 
-    # do GObject install
-    os.system(
-        "cd "+packages_dir+"; "
-        "tar xf pygobject-3.22.0.tar.xz; "
-        "cd pygobject-3.22.0; "
-        "sed -i '13273s/${prefix}/"+python_dir.replace("/", "\\/")+"/' configure; "
-        "sed -i '13275s/${exec_prefix}/"+python_dir.replace("/", "\\/")+"/' configure; "
-        "./configure; "
-        "make; "
-        "sudo make install; ")
+# do dbus-python install
+os.system(
+    "cd "+packages_dir+"; "
+    "tar xf dbus-python-1.2.4.tar.gz; "
+    "cd dbus-python-1.2.4; "
+    "sed -i '12352s/${prefix}/"+python_dir+"/' configure; "
+    "sed -i '12354s/${exec_prefix}/"+python_dir+"/' configure; "
+    "./configure; "
+    "make; "
+    "sudo make install; "
+    "python setup.py install; ")
 
-    # do dbus-python install
-    os.system(
-        "cd "+packages_dir+"; "
-        "tar xf dbus-python-1.2.4.tar.gz; "
-        "cd dbus-python-1.2.4; "
-        "sed -i '12352s/${prefix}/"+python_dir.replace("/", "\\/")+"/' configure; "
-        "sed -i '12354s/${exec_prefix}/"+python_dir.replace("/", "\\/")+"/' configure; "
-        "./configure; "
-        "make; "
-        "sudo make install; ")
- 
-    # do SBC decoder install
-    os.system(
-        "cd "+packages_dir+"; "
-        "tar xf sbc-1.2.tar.gz; "
-        "cd sbc-1.2; "
-        "./configure; "
-        "make; "
-        "sudo make install; ")
-    
-    setup(
-        name="pytooth",
-        version="1.0.0",
-        description="Linux-only Bluez5-based implementation of A2DP and HFP.",
-        long_description=long_description,
-        author="Anthony Ishkan",
-        author_email="anthony.ishkan@gmail.com",
-        url="https://bitbucket.org/ishkanan/pytooth",
-        packages=find_packages(exclude=["pytooth.tests"]),
-        package_data={
-            "": ["*.txt", "*.rst", "*.md"]
-        },
-        tests_require=test_requirements,
-        install_requires=install_requirements,
-        dependency_links=dependency_requirements,
-        entry_points={
-            "console_scripts": [
-                "pytooth-test = pytooth.tests.main:main",
-            ],
-        },
-        zip_safe=False
-    )
+# do SBC decoder install
+os.system(
+    "cd "+packages_dir+"; "
+    "tar xf sbc-1.2.tar.gz; "
+    "cd sbc-1.2; "
+    "./configure; "
+    "make; "
+    "sudo make install; ")
 
-    # clean up working folders
-    os.system(
-        "cd "+packages_dir+"; "
-        "sudo rm -Rf pygobject-3.22.0/; "
-        "sudo rm -Rf sbc-1.2/; "
-        "sudo rm -Rf dbus-python-1.2.4/; ")
+setup(
+    name="pytooth",
+    version="1.0.0",
+    description="Linux-only Bluez5-based implementation of A2DP and HFP.",
+    long_description=long_description,
+    author="Anthony Ishkan",
+    author_email="anthony.ishkan@gmail.com",
+    url="https://bitbucket.org/ishkanan/pytooth",
+    packages=find_packages(exclude=["pytooth.tests"]),
+    package_data={
+        "": ["*.txt", "*.rst", "*.md"]
+    },
+    tests_require=test_requirements,
+    install_requires=install_requirements,
+    dependency_links=dependency_requirements,
+    entry_points={
+        "console_scripts": [
+            "pytooth-test = pytooth.tests.main:main",
+        ],
+    },
+    zip_safe=False
+)
 
-if __name__ == '__main__':
-    main()
+# clean up working folders
+os.system(
+    "cd "+packages_dir+"; "
+    "sudo rm -Rf pygobject-3.22.0/; "
+    "sudo rm -Rf sbc-1.2/; "
+    "sudo rm -Rf dbus-python-1.2.4/; ")
