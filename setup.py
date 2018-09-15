305d71023298c486b490423217ed54d729fac2e4
Anthony Ishkan <anthony.ishkan@gmail.com>
2018-09-15 18:54:36 +1000

    Update setup.py to use pygobject==3.30.1

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ print("packages_dir = {}\nsrc_dir = {}".format(
 # compile and install sources
 # NOTE: cannot use setup.py for these since cannot pass -prefix and -exec-prefix
 sources = [
-    ("pygobject", "3.22.0"),
+    ("pygobject", "3.30.1"),
     ("dbus-python", "1.2.8"),
     ("sbc", "1.2.0")
 ]
